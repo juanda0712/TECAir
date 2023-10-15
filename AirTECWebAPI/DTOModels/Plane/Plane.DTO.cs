2cbda766915582d4a20d24cca701a117a68168e5
Meibel Ceciliano <cecilianomeibel@gmail.com>
2023-10-15 01:33:14 -0700

    Endpoints Plane

---
+++ AirTECWebAPI/DTOModels/Plane/Plane.DTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AirTECWebAPI.DTOModels.Plane
+{
+    public class PlaneDTO
+    {
+        public string PlateNumber { get; set; }
+        public int? ColumnNumber { get; set; }
+        public int? RowNumber { get; set; }
+    }
+}
+
+
