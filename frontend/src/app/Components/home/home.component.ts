776955aa8b31b597979c6c5f67b7459694ce7e37
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2023-10-16 22:32:22 -0600

    Home component conection with API [Issues]

--- frontend/src/app/Components/home/home.component.ts
+++ frontend/src/app/Components/home/home.component.ts
@@ -1,72 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { Router } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { Airport } from 'src/app/Interfaces/airport';
 import { ApiService } from 'src/app/Services/api-service';
-
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
+  standalone: true,
+  imports:[NgSelectModule, FormsModule],
+  styleUrls: ['../../../../src/styles.css']
 })
-export class HomeComponent {
-  branchList: string[] = [];
-  selectedOrigen: string = ''; // Variable para almacenar la zona de origen
-  selectedDestino: string = ''; // Variable para almacenar la zona de destino
+export class HomeComponent implements OnInit {
 
-  constructor(private api: ApiService<Airport>) { }
+  locationsList: Airport[] = [];
+  origen: Airport[] = [];
+  destino: Airport[] = [];
+  selectedOption: any;
+
+  constructor(private api: ApiService<Airport>, private router: Router) { }
 
   ngOnInit() {
     this.api.getAll('Airport').subscribe(
-      (airportList: Airport[]) => {
-        this.branchList = airportList.map((airport) => airport.City || '');
+      (locationsList: Airport[]) => {
+        this.locationsList = locationsList;
+        this.origen = [...locationsList]; // Copia de locationsList a origen
+        this.destino = [...locationsList]; // Copia de locationsList a destino
       },
       (error: any) => {
-        console.error('Error fetching branch:', error);
+        console.error('Error fetching locations:', error);
       }
     );
   }
 
-  onOrigenChange(event: any) {
-    this.selectedOrigen = event.target.value;
-  }
-
-  onDestinoChange(event: any) {
-    this.selectedDestino = event.target.value;
-  }
-
-  continuar() {
-    // Lógica para continuar con las selecciones de origen y destino
-    console.log('Origen seleccionado:', this.selectedOrigen);
-    console.log('Destino seleccionado:', this.selectedDestino);
+  selectDate() {
+    this.router.navigate(['/date']);
   }
 }
-
-
-
-
-/*
-export class HomeComponent {
-    origen = [
-        { value: 'opcion1', label: 'Opción 1' },
-        { value: 'opcion2', label: 'Opción 2' },
-        { value: 'opcion3', label: 'Opción 3' },
-      ];
-
-    destino = [
-        { value: 'opcion1', label: 'Opción 1' },
-        { value: 'opcion2', label: 'Opción 2' },
-        { value: 'opcion3', label: 'Opción 3' },
-    ]
-    
-    selectedOption: any; // Variable para almacenar la opción seleccionada
-
-    constructor(private router: Router) { }
-
-    selectDate() {
-      this.router.navigate(['/date']);
-    }
-
-}
-*/
