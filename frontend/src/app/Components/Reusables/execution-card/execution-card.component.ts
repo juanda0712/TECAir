f96dc3964824a42e600b6fbd515131b7422e71d5
Juan Rodriguez <juanrm071299@gmail.com>
2023-10-19 23:38:02 -0600

    [ADD] - execution and execution-card completed

--- frontend/src/app/Components/Reusables/execution-card/execution-card.component.ts
+++ frontend/src/app/Components/Reusables/execution-card/execution-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Execution } from 'src/app/Interfaces/execution';
 
 @Component({
@@ -8,4 +9,16 @@ import { Execution } from 'src/app/Interfaces/execution';
 })
 export class ExecutionCardComponent {
   @Input() execution!: Execution;
+  @Input() origin!: String;
+  @Input() destination!: String;
+
+  constructor(private router: Router) {}
+
+  booking() {
+    this.router.navigate(['/booking'], {
+      queryParams: {
+        idexecution: this.execution.idexecution,
+      },
+    });
+  }
 }
